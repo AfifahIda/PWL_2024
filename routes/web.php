018b318742d9e4fa6cbbe98ada3b3393f4e4338a
Afifah Ida Setyo Rini <afifah.ida.a57@gmail.com>
2024-09-02 10:10:04 +0700

    Praktikum 1 - Js 2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,47 @@
 
 use Illuminate\Support\Facades\Route;
 
+//basic routing
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM : 2241760032 <br> Nama : Afifah Ida Setyo Rini';
+});
+
+
+//routing parameters
+Route::get('/user/{nama}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comments}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+
+//optional parameters
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +53,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
